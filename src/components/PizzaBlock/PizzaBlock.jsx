4d9e1ddb801bf4ecd6d35cf7b304abaeaf90fb33
konstantine-sa <konstantine.sa@gmail.com>
2023-11-04 13:03:54 +0100

    PizzaItems component removed

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
+
 import PizzaBlockTopMenu from "./PizzaBlockTopMenu/PizzaBlockTopMenu";
-import PizzaItems from "./PizzaItems";
 import PizzaTitle from "./PizzaTitle";
+import PizzaItem from "./PizzaItem";
+import PizzaBlockSkeleton from "./PizzaBlockSkeleton";
 
 function PizzaBlock() {
+  const [pizzas, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://654082b145bedb25bfc20558.mockapi.io/pizza")
+      .then((res) => res.json())
+      .then((json) => {
+        setPizzas(json);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
       <PizzaBlockTopMenu />
       <PizzaTitle />
-      <PizzaItems />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-10 justify-center justify-items-center ">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => (
+              <PizzaBlockSkeleton key={index} />
+            ))
+          : pizzas.map((pizza) => <PizzaItem key={pizza.id} {...pizza} />)}
+      </div>
     </div>
   );
 }
