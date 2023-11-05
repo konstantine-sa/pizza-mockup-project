97d3fb0018e8c7190f3702c413d04302a7dd92d9
konstantine-sa <konstantine.sa@gmail.com>
2023-11-05 19:31:01 +0100

    PizzaBlock sort and filter logic added

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -9,19 +9,36 @@ function PizzaBlock() {
   const [pizzas, setPizzas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const [categoryId, setCategoryId] = useState(0);
+  const [filterSelected, setFilterSelected] = useState({
+    name: "Beliebtheit",
+    sortProperty: "rating",
+    sortWay: "desc",
+  });
+
   useEffect(() => {
-    fetch("https://654082b145bedb25bfc20558.mockapi.io/pizza")
+    setIsLoading(true);
+    fetch(
+      `https://654082b145bedb25bfc20558.mockapi.io/pizza?${
+        categoryId > 0 ? `category=${categoryId}` : ""
+      }&sortBy=${filterSelected.sortProperty}&order=${filterSelected.sortWay}`
+    )
       .then((res) => res.json())
       .then((json) => {
         setPizzas(json);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, filterSelected]);
 
   return (
     <div>
-      <PizzaBlockTopMenu />
+      <PizzaBlockTopMenu
+        categoryId={categoryId}
+        onClickCategory={(i) => setCategoryId(i)}
+        filterSelected={filterSelected}
+        setFilterSelected={(i) => setFilterSelected(i)}
+      />
       <PizzaTitle />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-10 justify-center justify-items-center ">
         {isLoading
--- src/components/PizzaBlock/PizzaBlockTopMenu/PizzaBlockTopMenu.jsx
+++ src/components/PizzaBlock/PizzaBlockTopMenu/PizzaBlockTopMenu.jsx
@@ -1,11 +1,137 @@
-import Categories from "./Categories";
-import Filter from "./Filter";
+import { useState, useEffect } from "react";
 
-function PizzaBlockTopMenu() {
+import CategoriesSkeleton from "./CategoriesSkeleton";
+
+function PizzaBlockTopMenu({
+  categoryId,
+  onClickCategory,
+  filterSelected,
+  setFilterSelected,
+}) {
+  const [categories, setCategories] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const filterList = [
+    {
+      name: "Beliebtheit",
+      sortProperty: "rating",
+      sortWay: "desc",
+    },
+    {
+      name: "Preis ↑",
+      sortProperty: "price",
+      sortWay: "asc",
+    },
+    {
+      name: "Preis ↓",
+      sortProperty: "price",
+      sortWay: "desc",
+    },
+    {
+      name: "Alphabet A-Z",
+      sortProperty: "name",
+      sortWay: "asc",
+    },
+    {
+      name: "Alphabet Z-A",
+      sortProperty: "name",
+      sortWay: "desc",
+    },
+  ];
+
+  console.log(filterSelected);
+
+  const onClickSelectFilter = (index) => {
+    setFilterSelected(index);
+    setIsVisible(!isVisible);
+  };
+
+  useEffect(() => {
+    fetch("https://654082b145bedb25bfc20558.mockapi.io/categories")
+      .then((res) => res.json())
+      .then((json) => {
+        setCategories(json);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <section className="flex flex-col lg:flex-row items-center justify-between w-full ">
-      <Categories />
-      <Filter />
+      {/* categories menu */}
+      <div>
+        <ul className="flex flex-wrap gap-3 justify-center">
+          {isLoading ? (
+            <CategoriesSkeleton />
+          ) : (
+            categories.map((categoryName, i) => (
+              <li
+                key={i}
+                onClick={() => onClickCategory(i)}
+                className={
+                  categoryId === i
+                    ? "py-1 px-4 md:py-3 md:px-7 font-semibold text-sm sm:text-base rounded-3xl cursor-pointer bg-[#282828] text-white"
+                    : "bg-[#f9f9f9] py-1 px-4 md:py-3 md:px-7 text-[#232323] text-sm font-semibold sm:text-base rounded-3xl cursor-pointer hover:bg-[#e0dddd] duration-150 ease-in-out "
+                }
+              >
+                {categoryName.name}
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+
+      {/* Filtering */}
+      <div className="relative mt-5 lg:mt-0">
+        {/* filter label */}
+        <div className="flex">
+          <div className="flex  items-center ">
+            <svg
+              width="10"
+              height="6"
+              fill="none"
+              className={isVisible ? "mr-2 rotate-180" : "mr-2"}
+            >
+              <path
+                d="M10 5a.6.6 0 0 1-.186.44.6.6 0 0 1-.439.185H.625a.6.6 0 0 1-.44-.186A.6.6 0 0 1 0 5a.6.6 0 0 1 .186-.44L4.56.187A.6.6 0 0 1 5 0a.6.6 0 0 1 .44.186L9.813 4.56A.6.6 0 0 1 10 5Z"
+                fill="#2C2C2C"
+              />
+            </svg>
+            <b className="mr-2  font-bold text-sm sm:text-base">
+              Sortieren nach:
+            </b>
+            <span
+              onClick={() => setIsVisible(!isVisible)}
+              className="text-[#fe5f1e]  border-b border-[#fe5f1e] border-dashed cursor-pointer"
+            >
+              {filterSelected.name}
+            </span>
+          </div>
+        </div>
+
+        {/* filter popup */}
+
+        {isVisible && (
+          <div className="absolute w-64 right-0 mt-4 pt-2 bg-white shadow-md rounded-lg">
+            <ul className="">
+              {filterList.map((obj, index) => (
+                <li
+                  key={index}
+                  onClick={() => onClickSelectFilter(obj)}
+                  className={
+                    filterSelected.name === obj.name
+                      ? "px-5 py-3 cursor-pointer text-[#fe5f1e] bg-[rgba(254,95,30,0.05)]"
+                      : "px-5 py-3 cursor-pointer hover:text-[#fe5f1e]"
+                  }
+                >
+                  {obj.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </section>
   );
 }
